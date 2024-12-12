c87f103b27e73aec318b058f7a9eb83ef184c459
MadduxLofton <125148911+MadduxLofton@users.noreply.github.com>
2024-12-12 07:39:15 -0600

    claim form update

--- HCI FEDEX Project/employee.js
+++ HCI FEDEX Project/employee.js
@@ -23,30 +23,57 @@ function claim_job()
                 'Job Claim Form' +
                 '<button class="popup-exit" onclick="delete_popup();">x</button>' +
             '</div>' +
-            'Name <br>' +
-            '<input type="text" name="employee_1st_name" id="employee_1st_name" placeholder="First" width="100%">' +
-            '<br>' +
-            '<input type="text" name="employee_last_name" id="employee_last_name" placeholder="Last">' +
-            '<br><br>' +
-            'Email<br>' +
-            '<input type="email" name=employee_email" id="employee_email placeholder="@gmail.com"> <br><br>' +
-            'Phone<br>' +
-            '<input type="tel" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"> <br><br>' +
-            'Address <br>' +
-            '<input type="text" name="employee_st_address" id="employee_st_address" placeholder="Street Address"> <br>' +
-            '<input type="text" name="employee_st_address2" id="employee_st_address2" placeholder="Street Address Line 2">  <br>' +
-            '<input type="text" name="employee_city" id="employee_city" placeholder="City"><br>' +
-            '<input type="text" name="employee_region" id="employee_region" placeholder="Region"> <br>' +
-            '<input type="text" name="employee_zip" id="employee_zip" placeholder="zip code"><br>' +
-            '<select name="country-dropdown" id="country-dropdown">' +
-                '<option value="United States">United States</option>' +
-                '<option value="Canada">Canada</option>' +
-                '<option value="Mexico">Mexico</option>' +
-                '<option value="United Kingdom">United Kingdom</option>'+
-            '</select> <br><br>' +
-            'Certification/Skills <br>' +
-            '<input type="text" name="employee_skills" id="employee_skills"> <br><br>' +
-        
+  
+            '<div class="job-claim-left-block">' +
+                'First Name <br>' +
+                '<div class="full-width">' +
+                    '<input type="text" name="employee_1st_name" id="employee_1st_name" placeholder="First" width="100%">' +
+                '</div>' +
+                '<br><br>Email<br>' +
+                '<div class="full-width">' +
+                    '<input type="text" name=employee_email" id="employee_email placeholder="@gmail.com"> <br><br>' +
+                '</div>' +
+            '</div>' +
+            '<div class="job-claim-right-block">' +
+                'Last Name<br>' +
+                '<div class="full-width">' +
+                    '<input type="text" name="employee_last_name" id="employee_last_name" placeholder="Last">' +
+                '</div>' +
+                '<br><br>Phone<br>' +
+                '<div class="full-width">' +
+                    '<input type="text" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"> <br><br>' +
+                '</div>' +
+            '</div>' +
+            '<div class="job-claim-form-text-section">' +    
+                'Address <br>' +
+                '<div class="full-width">' +
+                    '<input type="text" name="employee_st_address" id="employee_st_address" placeholder="Street Address"> <br>' +
+                    '<input type="text" name="employee_st_address2" id="employee_st_address2" placeholder="Street Address Line 2">  <br>' +
+                '</div>' +
+            '</div>' +
+            '<div class="job-claim-left-block">' +
+                '<div class="full-width">' +
+                    '<input type="text" name="employee_city" id="employee_city" placeholder="City"><br>' +
+                '</div>' +
+                '<div class="full-width">' +
+                    '<input type="text" name="employee_zip" id="employee_zip" placeholder="zip code"><br>' +
+                '</div>' +
+            '</div>'+ 
+            '<div class="job-claim-right-block">' +
+                '<div class="full-width">' +
+                    '<input type="text" name="employee_region" id="employee_region" placeholder="Region"> <br>' +
+                '</div>' +
+                '<select name="country-dropdown" id="country-dropdown">' +
+                    '<option value="United States">United States</option>' +
+                    '<option value="Canada">Canada</option>' +
+                    '<option value="Mexico">Mexico</option>' +
+                    '<option value="United Kingdom">United Kingdom</option>'+
+                '</select> <br><br>' +
+            '</div>' +
+            '<div class="job-claim-form-text-section">' +
+                'Certification/Skills <br>' +
+                '<textarea id="certs" name="certs" rows="4"></textarea> <br><br>' +
+            '</div>' +
             '<button class="use-profile-information-button" onclick="delete_popup();">Use Profile Information</button><br><br>' +
             '<button class="claim-job-submit" onclick="delete_popup();">Submit</button>' +
         '</div>' +
